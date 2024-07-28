590e2acfa7ef9a41a6c642ba78d7909654dea760
IvanMagda <ivanmagda164@gmail.com>
2024-07-28 19:13:04 +0300

    Initial Commit

---
+++ ChatMe/ChatMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeApp.swift
+//  ChatMe
+//
+//  Created by Ivan Magda on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
